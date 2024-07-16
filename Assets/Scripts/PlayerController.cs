d064a88d1580a7bea2123d5457425043aa2fd079
Noah Reed <Nore5515@gmail.com>
2024-07-16 11:15:22 -0700

    blocking tiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    Tilemap worldTilemap;
+
     [SerializeField]
     GameObject playerPrefab;
 
@@ -19,23 +23,59 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            pos.x += 1;
-            transform.position = pos;
+            if (IsTileAtVectorWalkable(pos + Vector3.right))
+            {
+                pos.x += 1;
+                transform.position = pos;
+            }
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            pos.x -= 1;
-            transform.position = pos;
+            if (IsTileAtVectorWalkable(pos - Vector3.right))
+            {
+                pos.x -= 1;
+                transform.position = pos;
+            }
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            pos.y += 1;
-            transform.position = pos;
+            if (IsTileAtVectorWalkable(pos + Vector3.up))
+            {
+                pos.y += 1;
+                transform.position = pos;
+            }
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            pos.y -= 1;
-            transform.position = pos;
+            if (IsTileAtVectorWalkable(pos - Vector3.up))
+            {
+                pos.y -= 1;
+                transform.position = pos;
+            }
         }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LogTileStandingOn();
+        }
+    }
+
+    bool IsTileAtVectorWalkable(Vector3 worldCoord)
+    {
+        string tileName = worldTilemap.GetTile(worldTilemap.WorldToCell(worldCoord)).name;
+
+        switch (tileName)
+        {
+            case "RedTile":
+                return false;
+            case "GreenTile":
+                return false;
+            default:
+                return true;
+        }
+    }
+
+    void LogTileStandingOn()
+    {
+        Debug.Log(worldTilemap.GetTile(worldTilemap.WorldToCell(pos)));
     }
 }
