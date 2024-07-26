9afe59723b280496c3e3920ed61face07306e498
Noah Reed <Nore5515@gmail.com>
2024-07-26 15:12:47 -0700

    added victory screen

---
+++ Assets/VictoryCanvas.cs
@@ -0,0 +1,14 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class VictoryCanvas : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
